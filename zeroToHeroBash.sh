a6b0c938d75726307d688641a6f91964f4a3f686
shaheer Qureshi <54498612+shaheerqureshi@users.noreply.github.com>
2023-03-02 20:58:45 -0500

    start Bashing the Bash

---
+++ zeroToHeroBash.sh
@@ -0,0 +1,10 @@
+User Name:
+(sysadmin)@localhost:~$
+
+System Name:
+sysadmin@(localhost):~$
+
+Current Directory:
+sysadmin@localhost:(~)$
+The ~ symbol is used as shorthand for the user's home director
+
